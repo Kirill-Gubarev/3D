c47be02e3f78192acb0777bde0bab5fe29bdd308
Kirill <kirillgubarev5667@gmail.com>
2024-03-31 18:29:11 +0700

    libraries were connected

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <GLAD/glad.h>
+#include <GLFW/glfw3.h>
 int main(void){
-	std::cout<<"Monkey"<<std::endl;
-	system("pause");
+	if (!glfwInit()){
+		std::cout << "glfw has not been initialized" << std::endl;
+		return -1;
+	}
+	GLFWwindow* pWindow = glfwCreateWindow(700,500,"3D",nullptr,nullptr);
+	if (!pWindow){
+		std::cout << "window has not been created" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(pWindow);
+	if (!gladLoadGL()) {
+		std::cout << "glad has not been loaded" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glClearColor(0,1,0,1);
+	while (!glfwWindowShouldClose(pWindow))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(pWindow);
+		glfwPollEvents();
+	}
+	glfwTerminate();
 	return 0;
 }
